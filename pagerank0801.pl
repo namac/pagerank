6e466c752c8f170fb3d7cb4460618e71a6ed2401
Na <mylife.hispec.mac@gmail.com>
2014-08-05 22:53:08 +0900

    規格化適応後

--- pagerank0801.pl
+++ pagerank0801.pl
@@ -11,7 +11,7 @@ __PACKAGE__->mk_accessors(qw/neighbor_matrix initial_vector iteration/);
 use PDL;
 use List::Util;
 
-sub proc_eigen_vector{
+sub calc{
 	my $self = shift;
 	
 
@@ -24,25 +24,31 @@ sub proc_eigen_vector{
 	#printf $vec;
 	#printf $p x $vec;
 
-	#行列の計算 iteratorが45回で収束
 	for(my $i = 0; $i < $self->iteration; $i++){
 			$vec = $p x $vec;
-			printf $i;
-			printf $vec;
 	}
+	return $vec / @{$self->initial_vector};
 }
+
 package main;
 
 my $pr = PageRank->new;
 
+# 隣接行列
 $pr->neighbor_matrix = [
-	[0, 1, 0],
-	[0.5, 0, 0.5],
-	[1, 0, 0]
+	[0, 0.2, 0.2, 0.2, 0.2, 0, 0.2],
+	[1, 0, 0, 0, 0, 0, 0],
+	[0.5, 0.5, 0, 0, 0, 0, 0],
+	[0, 1/3, 1/3, 0, 1/3, 0, 0],
+	[0.25, 0, 0.25, 0.25, 0, 0.25, 0],
+	[0.5, 0, 0, 0, 0.5, 0, 0],
+	[0, 0, 0, 0, 1, 0, 0]
 ];
 
-$pr->initial_vector = [1, 1, 1];
-$pr->iteration = 50;
-$pr->proc_eigen_vector;
 
+$pr->initial_vector = [1, 1, 1, 1, 1, 1, 1];
+$pr->iteration = 100;
+my $res = $pr->calc;
+
+printf $res;
 	
